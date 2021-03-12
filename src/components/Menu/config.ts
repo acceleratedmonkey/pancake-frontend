32ee11968d2bc66638c29661eedffdf421f7055a
Johanes Cristanto <johanes.cristanto@grabtaxi.com>
2021-03-12 17:41:40 +0800

    update config

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -2,9 +2,19 @@ import { MenuEntry } from 'uikit'
 
 const config: MenuEntry[] = [
   {
-    label: 'Home',
-    icon: 'HomeIcon',
-    href: '/',
+    label: 'Trade',
+    icon: 'TradeIcon',
+    initialOpenState: true,
+    items: [
+      {
+        label: 'Exchange',
+        href: 'https://impossible-swap-v1.netlify.app/swap',
+      },
+      {
+        label: 'Liquidity',
+        href: 'https://impossible-swap-v1.netlify.app/pool',
+      },
+    ],
   },
   {
     label: 'Farms',
